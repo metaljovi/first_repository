813bf41fe5f8a4e97977004652a7cea8217aeacb
metaljovi <fajarmp@gmail.com>
2016-11-17 10:59:59 +0000

    3rd commit : add wsd.py

---
+++ wsd.py
@@ -0,0 +1,22 @@
+import requests
+import re
+import IPython
+
+def wsd(code):
+    response = requests.post("http://www.websequencediagrams.com/index.php", data={
+            'message': code,
+            'apiVersion': 1,
+        })
+    expr = re.compile("(\?(img|pdf|png|svg)=[a-zA-Z0-9]+)")
+    m = expr.search(response.text)
+    if m == None:
+        print("Invalid response from server.")
+        return False
+                            
+    image=requests.get("http://www.websequencediagrams.com/" + m.group(0))
+    return IPython.core.display.Image(image.content)
+
+message= """Working Directory -> Staging Area : git add 
+	    Staging Area -> Local Repository : git commit
+	    Working Directory -> Local Repository : git commit -a"""
+wsd(message)
